69356cb0d5dbab13c1c573e4060a0f6e4b241e57
AleX04Nov <alex.novikov@cossacklabs.com>
2022-05-30 20:03:30 +0300

    review fixes
    Cargo:
    -- added `anyhow = "1.0"` crate.
    
    Main:
    -- added return type and return value from main;
    -- slightly changed print value;
    -- added more `quit` strings;
    -- changed input error handling;
    -- removed explicit types for variables.
    
    Uglifier:
    -- changed `return` syntax from functions;
    -- changed strings formatting;
    -- removed explicit types for variables.

--- key-uglifier/src/main.rs
+++ key-uglifier/src/main.rs
@@ -1,19 +1,21 @@
 pub mod uglifier;
 
-fn main() {
-    let mut line: String = String::new();
+fn main() -> anyhow::Result<()> {
+    let mut line = String::new();
     loop {
         line.clear();
-        println!("Input key: \t");
-        std::io::stdin().read_line(&mut line).unwrap();
+        println!("Input key (16/24/32 bytes):");
+        std::io::stdin().read_line(&mut line)?;
         // remove new line char if present and leading and tracing spaces
         line = line.trim().to_string();
         match line.to_lowercase().as_str() {
-            "q" => { break },
+            "q" | "quit" | "exit" => break,
             _ => {
                 let encryption_key = uglifier::param_set_encryption_key(&line);
                 println!("{} - uglified key {}\n", encryption_key.0, encryption_key.1);
             }
         }
     }
+
+    Ok(())
 }
--- key-uglifier/src/uglifier.rs
+++ key-uglifier/src/uglifier.rs
@@ -1,62 +1,47 @@
 use colored::Colorize;
 use hex::ToHex;
+use std::fmt::Write;
 
-fn param_set_default_encryption_key(key_length: u8) -> Vec<u8>
-{
-    let default_key = vec![0x62u8; key_length as usize];
-    return default_key
+fn param_set_default_encryption_key(key_length: u8) -> Vec<u8> {
+    vec![0x62u8; key_length as usize]
 }
 
-fn read_hex_nibble(c: char) -> (bool, String)
-{
-    if (c >='0') && (c <= '9'){
-        return (true, c.to_string());
-    }
-    else if (c >='A') && (c <= 'F')
-    {
-        return (true, c.to_string());
-    }
-    else if (c >='a') && (c <= 'f')
-    {
-        return (true, c.to_string());
-    }
-    else
-    {
+fn read_hex_nibble(c: char) -> (bool, String) {
+    if "0123456789ABCDEFabcdef".contains(c) {
+        (true, c.to_string())
+    } else {
         // printf("[%u] read_hex_nibble: Error char not in supported range",nodeId);
-        return (false, '0'.to_string().on_color("green").to_string());
+        (false, '0'.to_string().on_color("green").to_string())
     }
 }
 
-fn convert_to_hex(key_input: &String, key_length: u8) -> (bool, String) {
+fn convert_to_hex(key_input: &str, key_length: u8) -> (bool, String) {
     let mut key_output = String::new();
-    let mut bad_chars: bool = false;
+    let mut bad_chars = false;
 
-    for c in key_input.chars().enumerate(){
-        if c.0 > ((key_length as usize) * 2) - 1 {
+    for (index, character) in key_input.chars().enumerate() {
+        if index > ((key_length as usize) * 2) - 1 {
             break;
         }
-        let num = read_hex_nibble(c.1);
-        key_output.push_str(&num.1);
-        bad_chars |= !num.0;
+        let (ok, num) = read_hex_nibble(character);
+        key_output.push_str(&num);
+        bad_chars |= !ok;
     }
 
-    return (bad_chars, key_output);
+    (bad_chars, key_output)
 }
 
-pub(crate) fn param_set_encryption_key(key_input: &String) -> (String, String)
-{
+pub(crate) fn param_set_encryption_key(key_input: &str) -> (String, String) {
     // Use the new encryption level to help with key changes before reboot
     // Deduce key length (bytes) from level 1 -> 16, 2 -> 24, 3 -> 32
-    let len: u8 = key_input.len() as u8;
-    let key_length: u8;
+    let len = key_input.len() as u8;
+    let key_length;
 
     if len >= 32 * 2 {
         key_length = 32;
-    }
-    else if len >= 24 * 2{
+    } else if len >= 24 * 2 {
         key_length = 24;
-    }
-    else {
+    } else {
         key_length = 16;
     }
 
@@ -64,7 +49,7 @@ pub(crate) fn param_set_encryption_key(key_input: &String) -> (String, String)
     // of his hand:
     // warning: value assigned to `output_key` is never read
     let mut output_key: String;
-    let mut output_str_desc: String = String::new();
+    let mut output_str_desc = String::new();
 
     // If not enough characters (2 char per byte), then set default
     if len < 2 * key_length {
@@ -75,33 +60,47 @@ pub(crate) fn param_set_encryption_key(key_input: &String) -> (String, String)
             .bold()
             .to_string();
         //println!("{}\n",output_key);
-        output_str_desc += &*"- using default key\n".to_string().bold().to_string();
-        output_str_desc += &*" ERROR ".to_string().color("white").on_color("red").bold().to_string();
-        output_str_desc += &*format!(" - Key length: {}, Required: {}", len, 2 * key_length);
+
+        write!(
+            output_str_desc,
+            "{}{} - Key length (in hex chars): {}, Required (in hex chars): {}",
+            "- using default key\n".bold(),
+            " ERROR ".color("white").on_color("red").bold(),
+            len,
+            2 * key_length
+        )
+        .expect("Write to a string cannot fail");
+
         //output_str_desc = format!("- using default key, because:\n ERROR - Key length:{}, Required {}\n".red(), len, 2 * key_length);
         return (output_key, output_str_desc);
-    }
-    else {
+    } else {
         // We have sufficient characters for the encryption key.
         // If too many characters, then it will just ignore extra ones
         //println!("key len {}\n",key_length);
         let bad_chars: bool;
         (bad_chars, output_key) = convert_to_hex(key_input, key_length);
         if bad_chars {
-            output_str_desc += &*"\n".to_string();
-            output_str_desc += &*" WARNING ".to_string().color("black").on_color("bright yellow").bold().to_string();
-            output_str_desc += &*" Wrong chars appeared!";
+            write!(
+                output_str_desc,
+                "\n{} Wrong chars appeared!",
+                " WARNING ".color("black").on_color("bright yellow").bold()
+            )
+            .expect("Write to a string cannot fail");
         }
         if len > (key_length * 2) {
             let trimmed_elements_count = len - (key_length * 2);
-            let trimmed_bytes: String = " ".repeat(trimmed_elements_count as usize).on_color("red").to_string();
-            output_key += &*trimmed_bytes;
+            let trimmed_bytes = " ".repeat(trimmed_elements_count as usize).on_color("red");
+            write!(output_key, "{}", trimmed_bytes).expect("Write to a string cannot fail");
 
-            output_str_desc += &*"\n".to_string();
-            output_str_desc += &*" WARNING ".to_string().color("black").on_color("bright yellow").bold().to_string();
-            output_str_desc += &*format!(" Too long input key. It was trimmed! {} chars were trimmed!", trimmed_elements_count);
+            write!(
+                output_str_desc,
+                "\n{} Too long input key. It was trimmed! {} hex chars were trimmed!",
+                " WARNING ".color("black").on_color("bright yellow").bold(),
+                trimmed_elements_count
+            )
+            .expect("Write to a string cannot fail");
         }
     }
 
-    return (output_key, output_str_desc);
+    (output_key, output_str_desc)
 }
